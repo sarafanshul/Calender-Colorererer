c4fbad97251f12b087d9744bfd4cdb549444de3a
sarafanshul <anshulsaraf3@gmail.com>
2019-12-24 17:59:01 +0530

    fancier output

--- src/bashscript.py
+++ src/bashscript.py
@@ -9,33 +9,34 @@ from random import randint
 from mk_copy import main_echo
 import json
 from shutil import copy
+from colorama import Fore, Back, init
 
 # Set-ExecutionPolicy -ExecutionPolicy Unrestricted
 # p = subprocess.Popen(['powershell.exe', command], stdout=sys.stdout)
 
 count_c = 0
 
-def echo(base_scr ,base_D):
+def echo(base_scr:str ,base_D:str) -> str:
 	global count_c
 	filename = main_echo(base_scr ,base_D ,count_c)
 	count_c += 1
 	return filename
 
 
-def create(arg):
+def create(arg:int) -> 'Explicit Return':
 	create_here = subprocess.run(['mkdir' ,str(arg)] ,shell = True)
 
 
-def cd_in(base ,local):
+def cd_in(base:str ,local:str) -> str:
 	os.chdir(base+local)
 	return base + local +'//'
 
 
-def cd_out(par_base):
+def cd_out(par_base:str) -> 'Explicit Return':
 	os.chdir(par_base)
 
 
-def ps_script(Y ,M ,D ,H ,file ,base_cur):
+def ps_script(Y:int ,M:int ,D:int ,H:int ,file:str ,base_cur:str) -> 'Explicit Return':
 	filename = file
 	file = base_cur + file
 	proc = subprocess.Popen([r'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe',
@@ -46,13 +47,13 @@ def ps_script(Y ,M ,D ,H ,file ,base_cur):
 	proc.wait()
 
 
-def call_script(base_scr ,Y ,M ,D ,H ,file ,base_cur):
+def call_script(base_scr:str ,Y:int ,M:int ,D:int ,H:int ,file:str ,base_cur:str) _. 'Explicit Return:
 	os.chdir(base_scr)
 	ps_script(Y ,M ,D ,H ,file ,base_cur) #searching on main
 	os.chdir(base_cur)
 
 
-def cleanup():
+def cleanup() -> 'Explicit Return':
 	proc_clean = subprocess.Popen([r'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe',
                             '-ExecutionPolicy','Bypass',
                             './script_clean.ps1'],stdout=sys.stdout)
@@ -65,6 +66,8 @@ def main():
 	floder = 'main_dir'
 	create(floder)
 	base = cd_in(base_scr ,floder)
+	# ste Y,M,D ranges correspondingly
+
 	for Y in range(2019 ,2020):
 		create(Y)
 		base_Y = cd_in(base ,str(Y))
@@ -80,6 +83,7 @@ def main():
 					base_D = cd_in(base_M ,str(D))
 
 					for H in range(0,8):
+
 						if randint(1,5) % 5 >= 2:
 							cmmts += 1
 							filename = echo(base_scr ,base_D)
@@ -94,10 +98,13 @@ def main():
 
 	cd_out(base_scr)
 	cleanup()
-	print(f'\n\n\nTotal commits -> {cmmts}')
+
+	print(Fore.WHITE ,Back.GREEN +f'\n\n\nTotal commits -> {cmmts}')
 
 
 if __name__ == '__main__':
+	os.system('color 9')
 	s = time()
 	main()
-	print(f'>>>time taken -> {time()-s}')
+	print(Fore.WHITE ,Back.GREEN + f'time taken -> {time()-s}')
+	print(Style.RESET_ALL)
--- src/json_dump.py
+++ src/json_dump.py
@@ -1,7 +1,15 @@
 import os
+import time
 import json
 
 def main_call(dr):
+	if not os.path.exists('dr'):
+		print('\n\n\n\n')
+		print('source folder not found ')
+		print('extract the source.rar file ')
+		print('press    "Ctrl + C" to exit')
+		time.sleep(1000)
+
 	contents = os.listdir(dr)
 	with open('json_dump_src.json' ,'w') as jsn:
 		json.dump(contents ,jsn)
